32a7dafc0221fd0ca7e5a2b99a3bcb64515285b6
hoanglong3nhe <hoanglong.3nhe@gmail.com>
2026-09-02 15:43:47 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,10 @@
-
+export default {
+  async fetch(request, env) {
+    return new Response("hotsocial.space worker is running", {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=UTF-8"
+      }
+    });
+  }
+};
